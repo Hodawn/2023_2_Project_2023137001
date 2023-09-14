5680145a965c8e7a9c24636f6b8f6a947feb2f65
Hodawn <happygy925@naver.com>
2023-09-14 10:50:23 +0900

    Feat: "Generic 싱글톤 구현"

---
+++ Project0907/Assets/Scripts/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : GenricSingleton<GameManager>
+{    public int playerScore = 0;                                 //관리할 플레이어 스코어
+
+    public void inscreaseScore(int amount)
+    {
+        playerScore += amount;                              //함수를 통해서 스코어를 증가시킨다.
+    }
+}
